ccfcfb0b9a69df38752c9bfaf31bd7c10db2d95b
Uwabunkeonye Ijeoma <uwabunkeonyeijeoma@gmail.com>
2026-01-30 12:18:38 +0100

    installatin guildelines

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
-#!/usr/bin/env bash
 set -e
 
-echo "⬇️ Downloading Precheck (v1.0.0-beta)..."
-
-curl -fsSL https://github.com/bennydreamtech23/precheck/releases/download/v1.0.0-beta/precheck_v1.0.0.tar.gz -o precheck.tar.gz
+echo "⬇️ Downloading Precheck (v1.0.0)..."
+curl -fsSL https://github.com/bennydreamtech23/precheck/releases/download/v1.0.0/precheck-v1.0.0-linux-x64.tar.gz -o precheck.tar.gz
 
 echo "📦 Extracting package..."
 tar -xzf precheck.tar.gz
 
 echo "⚙️ Installing..."
-chmod +x precheck
-sudo mv precheck /usr/local/bin/
+sudo mv bin/precheck /usr/local/bin/precheck
 
 echo "✅ Precheck installed successfully!"
+precheck --help
